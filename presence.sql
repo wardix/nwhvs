8c6514d1746156d89d8a8ca1cfc8972ba46149e1
Wardi <wardi@nusa.net.id>
2023-06-23 00:22:09 +0700

    remove sqlite database

--- presence.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS presence (
-  device VARCHAR(255),
-  employee_id VARCHAR(15),
-  employee_name VARCHAR(63),
-  clock DATETIME,
-  PRIMARY KEY (device, employee_id, clock)
-);
